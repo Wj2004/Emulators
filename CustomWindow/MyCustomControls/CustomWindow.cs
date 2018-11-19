9a80b297dd55d15c66810bfad68e919641d58ab1
William Jelgren <william@jelgren.dk>
2018-11-19 17:02:01 +0100

    Changed window back to Windows Standard

---
+++ CustomWindow/MyCustomControls/CustomWindow.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Shapes;
+
+namespace MyCustomControls
+{
+    public class CustomWindow : Window
+    {
+        static CustomWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomWindow),
+                new FrameworkPropertyMetadata(typeof(CustomWindow)));
+        }
+
+        public CustomWindow()
+            : base()
+        {
+            PreviewMouseMove += OnPreviewMouseMove;
+        }
+
+        protected void OnPreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton != MouseButtonState.Pressed)
+                Cursor = Cursors.Arrow;
+        }
+
+        #region Click events
+        protected void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+            //SystemCommands.MinimizeWindowCommand(this);
+        }
+ 
+        protected void RestoreClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+        }
+ 
+        protected void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        #endregion
+
+
+        public override void OnApplyTemplate()
+        {
+            if (GetTemplateChild("minimizeButton") is Button minimizeButton)
+                minimizeButton.Click += MinimizeClick;
+
+            if (GetTemplateChild("restoreButton") is Button restoreButton)
+                restoreButton.Click += RestoreClick;
+
+            if (GetTemplateChild("closeButton") is Button closeButton)
+                closeButton.Click += CloseClick;
+
+            if (GetTemplateChild("moveRectangle") is Rectangle moveRectangle)
+            {
+                moveRectangle.PreviewMouseDown += MoveRectangle_PreviewMouseDown;
+            }
+
+
+            if (GetTemplateChild("resizeGrid") is Grid resizeGrid)
+            {
+                foreach (UIElement element in resizeGrid.Children)
+                {
+                    if (element is Rectangle resizeRectangle)
+                    {
+                        resizeRectangle.PreviewMouseDown += ResizeRectangle_PreviewMouseDown;
+                        resizeRectangle.MouseMove += ResizeRectangle_MouseMove;
+                    }
+                }
+            }
+
+            base.OnApplyTemplate();
+        }
+
+        private void MoveRectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+
+        protected void ResizeRectangle_MouseMove(Object sender, MouseEventArgs e)
+        {
+            Rectangle rectangle = sender as Rectangle;
+            switch (rectangle.Name)
+            {
+                case "top":
+                    Cursor = Cursors.SizeNS;
+                    break;
+                case "bottom":
+                    Cursor = Cursors.SizeNS;
+                    break;
+                case "left":
+                    Cursor = Cursors.SizeWE;
+                    break;
+                case "right":
+                    Cursor = Cursors.SizeWE;
+                    break;
+                case "topLeft":
+                    Cursor = Cursors.SizeNWSE;
+                    break;
+                case "topRight":
+                    Cursor = Cursors.SizeNESW;
+                    break;
+                case "bottomLeft":
+                    Cursor = Cursors.SizeNESW;
+                    break;
+                case "bottomRight":
+                    Cursor = Cursors.SizeNWSE;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 msg, IntPtr wParam, IntPtr lParam);
+
+        protected void ResizeRectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rectangle = sender as Rectangle;
+            switch (rectangle.Name)
+            {
+                case "top":
+                    Cursor = Cursors.SizeNS;
+                    ResizeWindow(ResizeDirection.Top);
+                    break;
+                case "bottom":
+                    Cursor = Cursors.SizeNS;
+                    ResizeWindow(ResizeDirection.Bottom);
+                    break;
+                case "left":
+                    Cursor = Cursors.SizeWE;
+                    ResizeWindow(ResizeDirection.Left);
+                    break;
+                case "right":
+                    Cursor = Cursors.SizeWE;
+                    ResizeWindow(ResizeDirection.Right);
+                    break;
+                case "topLeft":
+                    Cursor = Cursors.SizeNWSE;
+                    ResizeWindow(ResizeDirection.TopLeft);
+                    break;
+                case "topRight":
+                    Cursor = Cursors.SizeNESW;
+                    ResizeWindow(ResizeDirection.TopRight);
+                    break;
+                case "bottomLeft":
+                    Cursor = Cursors.SizeNESW;
+                    ResizeWindow(ResizeDirection.BottomLeft);
+                    break;
+                case "bottomRight":
+                    Cursor = Cursors.SizeNWSE;
+                    ResizeWindow(ResizeDirection.BottomRight);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ResizeWindow(ResizeDirection direction)
+        {
+            SendMessage(_hwndSource.Handle, 0x112, (IntPtr)(61440 + direction), IntPtr.Zero);
+        }
+
+        private enum ResizeDirection
+        {
+            Left = 1,
+            Right = 2,
+            Top = 3,
+            TopLeft = 4,
+            TopRight = 5,
+            Bottom = 6,
+            BottomLeft = 7,
+            BottomRight = 8,
+        }
+
+        private HwndSource _hwndSource;
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            SourceInitialized += OnSourceInitialized;
+            base.OnInitialized(e);
+        }
+
+        private void OnSourceInitialized(object sender, EventArgs e)
+        {
+            _hwndSource = (HwndSource)PresentationSource.FromVisual(this);
+        } 
+    }
+
+}
--- Emulators/MainWindow.xaml.cs
+++ Emulators/MainWindow.xaml.cs
@@ -1,12 +1,11 @@
-﻿using MyCustomControls;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Emulators
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : CustomWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
